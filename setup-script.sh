bcfd24e9a745ea2ac71712d6b5b9f2bd79e8cc70
rod <>
2024-04-20 13:37:16 +0000

    renamed file
    FossilOrigin-Name: 0eab590830dc6357e5cc7252f772e38796d55d87e01d1a99e6236d9a1bbc35cc

--- setup-script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Installation of standard packages 
-sudo apt-get install syncthing magic-wormhole keepassxc ssh curl 
-
-# Creation of small scripts 
-## Updateme 
-sudo cp ./scripts/updateme /usr/bin/
