7c442558af625b667ed81bcc7883790822a481b5
rod <>
2024-04-20 13:40:35 +0000

    update
    FossilOrigin-Name: c148113eb30f5ff4ff60a278f3ae50b07d2947b13769ce6130edb61af946a15e

--- main-script.sh
+++ main-script.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 # Installation of standard packages 
-sudo apt-get install syncthing magic-wormhole keepassxc ssh curl 
+sudo apt-get install -y
+	# Basic utilities which are missing in some distros
+	ssh curl
+	# Networking 
+	syncthing magic-wormhole 
+	# GUI programs I like to use 
+	keepassxc 
 
 # Creation of small scripts 
 ## Updateme 
 sudo cp ./scripts/updateme /usr/bin/
+## Mount encrypted drive
+
